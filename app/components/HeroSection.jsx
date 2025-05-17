e3e761bb02b15fd1494f908fdc45d7a48b7cf62d
Rohit Kumar <rohit2@wisc.edu>
2025-05-17 16:09:42 -0500

    Updated resume

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -47,7 +47,7 @@ const HeroSection = () => {
                             Hire Me
                         </Link>
                         <Link
-                            href="https://drive.google.com/file/d/1M0-qkN67FCRAHY45skcaAsFzjzjKOuum/view?usp=sharing" target="_blank" rel="noopener noreferrer"
+                            href="https://drive.google.com/file/d/1mIZHOx47BH_uEtVQJzUbDHyc7VLsSEzQ/view?usp=sharing" target="_blank" rel="noopener noreferrer"
                             className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-purple-500 to-teal-500 hover:bg-slate-800 text-white mt-3"
                         >
                             <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
